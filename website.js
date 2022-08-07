9795b513d212c43cddfc20f6472f7c5d0e530fd0
not-JASH <jluna094@uottawa.ca>
2022-08-07 18:25:41 -0400

    Update website.js

--- website.js
+++ website.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const path = require("path");
-const controllers = require('./assets/js/controllers');
+const a = require('./assets/js/Controllers');
 
 
 
@@ -11,28 +11,24 @@ app.set("views",path.join(__dirname,"views"));
 app.engine('html',require('ejs').renderFile);
 app.set("view engine","ejs");
 
-const Home = new controllers.controller("Home.html");
-const Portfolio = new controllers.controller("Portfolio.html");
-const WorkExperience = new controllers.controller("WorkExperience.html");
-const WaveGAN = new controllers.controller("WaveGAN.html");
+
+const Home = new a.controller("Home.html");
+const Portfolio = new a.controller("Portfolio.html");
+const WorkExperience = new a.WorkExperience("workExperience.html");
+const WaveGAN = new a.WaveGAN("WaveGAN.html");
+
+
 
 function onHttpStart(){
     console.log("Server started, listening on " + HTTP_PORT); 
 }
 
-app.get("/", function(req,res) {
-    Home.display(req,res);
-})
-
-
+app.get("/",Home.display(req,res))
 
 app.get("/Portfolio", Portfolio.display(req,res))
+
 app.get("/Work-Experience", WorkExperience.display(req,res))
+
 app.get("/WaveGAN", WaveGAN.display(req,res))
-
-
-
-
-
 
 app.listen(HTTP_PORT,onHttpStart);
