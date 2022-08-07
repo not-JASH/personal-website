859e34d521d7152379ea2993018d55695c5aef0e
not-JASH <jluna094@uottawa.ca>
2022-08-06 20:27:31 -0400

    added startup script

---
+++ website.js
@@ -0,0 +1,22 @@
+const exp = require("constants");
+const express = require("express");
+const path = require("path");
+
+
+const app = express();
+const HTTP_PORT = process.env.PORT || 8080;
+app.use(express.static(__dirname+'/assets'));
+app.set("views",path.join(__dirname,"views"));
+app.engine('html',require('ejs').renderFile);
+app.set("view engine","ejs");
+
+// const controllers = require('./assets/js/controllers');
+
+function onHttpStart(){
+    console.log("Server started, listening on " + HTTP_PORT); 
+}
+
+
+app.get("/",function(req,res) {
+    res.render("Home")
+})
