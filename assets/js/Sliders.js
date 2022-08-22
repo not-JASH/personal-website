ac717b40fdd54923e44c36f7c3adad09e25a244a
not-JASH <jluna094@uottawa.ca>
2022-08-22 18:13:08 -0400

    some changes

--- assets/js/Sliders.js
+++ assets/js/Sliders.js
@@ -1,8 +1,59 @@
+const images = {
+  "Togetherness": {
+      "image":[
+          "https://chi01pap001files.storage.live.com/y4m6mSiTOL_hx5gXE_vi1RmsW-Fub_9tihTG_iG1PWbHoUqqFA0YMSvhPFQbfPLU5Q9zIzVZM-dr5o25rzoI0gnfXAZ8eyGs8JkMnzVZ08vaH2aKIqVhCjvpkFJ5oKr0P8G_LEmE95nhzydXgueZmMQyrJBtQe0uPaJpAYc-yF-RD9V8yWnkzv2_sGCP0kiReOr?width=3840&height=1974&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mpfoTg532KlodrPqJAxW3ZD4_Z8MH208j9qRwIDMtW7pYMrmqjJrC5TT2DONUtMUQhJ4Est9YOYnjYPNkWSkTOlZOTziWBxe86-kmPmM7pIJsZJXwacrjr_Em7cP2a_BuquGV_Auh9jTst_BKqP76wGfq7lBiuT8iY6xLywkql_BqP2qwo3RKsoJXuM5YmHyY?width=3840&height=1989&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4m6VCcl0bP6deWZpM3EAeKTQsLg7vd7B5NMvFYFZjX-laUmlnPITIs4U3XC1EieYuIFO7rJTWf6gHuCjdhrzR8Oo2uH2Noiwz3HbqBSQclNk_84ZhVYbzyWuU9eW_58QfBBvYPGPp3wC7h-yceC0BOpuE1SpJ63cj6aADnBkq0UTtcYp45_V51GRJG37ZLSPYp?width=3840&height=1989&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4m-W1AhpOemGO0NlFzvSOwy5UVmvrLDdxMX8L1PLIilqRwZL7lngpVf-zVBvHXoPYDROPKmFWu9iSdVs5-gpjtOnb_E-KyLQDPRW0_W5Asiz6xjxZ43onqeusoC4p5lWWpYd_winB8ixYc7yCOOB_wwcjbRyjXrStiYAsqfqDEud6K3zxRG3wGBdaaXfMP-sBO?width=3264&height=1672&cropmode=none"            
+      ]
+  },
+  "LivingBreathing": {
+      "image":[
+          "https://chi01pap001files.storage.live.com/y4mc5HRS9s-ads0zs9lyEyp1at2-nxrndr8z9vFHjWTkbjJ35GklClxAQ8fJs7Cb_QDo4hR4b0soB5Uq0_HV2rT-nRCRpRqHhFPq1a8ZDTfJlMvNH-mtO-UdMHR7IHY3Z_W9QHCXM2XGTypmfqP7SnktTdpjawm3WtR-_nMT5soX_aYdd_ujccB209GlrSL9SAz?width=2560&height=1326&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mDrrdee7lhoxrOvQXSWXSiuPKN_QBekrbcfj4T7aUoARVo-iMnWtRXTLy1BeYWNGpDLdAWOf5RomiLXNS1VX46dtHryTnvIgSoH_5YldZ2z6KRypZy8ODsE4meqvveXWteocR-5c6o1LidsxSiR9L-8ymL_cLFVjPYJfUNZy5QPomhcEOVM1BRp4tM9woo5WQ?width=3840&height=1995&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mw1JPc911-dG1Eeu32O6_WnxqdrdAbLFQ-xD6Zyot-f-9A87Bdm0Sd2HOqWM1I2OYOifAhuX5COkiuGC0QzxlIftzu6KgTWV4_-G3SvjRQAeD11DJwx5vwqQ5EztjlXhazTBZmmSQ-b_SAUDg7u2SRYP2ScL6tbJdagP10bCciQi1XPgtHhZldTm7KJbINwQa?width=3264&height=1692&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mk90reMWcCayQlIa7_vSpRJSKafM9fkBPA--vGVeOgnzmVJ4PTcViRwAoi52JWTLpmSCIZi3hw_o3WPv4XDYK1T94yFXlv9VGT_MLMWKH-CrxJSbcaScafpedn5YPJQnaQjx3ol6jZkmtqEQs22IAMnQygo0YqstGmTSyf9W3G72HhEw3G9x5CFv-oDfPDAhN?width=3840&height=1971&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mTABvIgBj13eotsJnuR_MtY714AhEhEm18mqJuzlALXg0RhEpvRU7TRIf-znFgYY03OD5Rq_m8zyYSmat36YPyAibC59rViPYz0zkPzoSTrA6xA3J9Kk-1bPTQrkSXCKKxgSioWcaMRJHBZfzXUUvKwq0b6mbIdbVRE0tJn4UvBRVGeAzVYE5xmfsv0XPh_7g?width=3840&height=1995&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mBfvAu5k-yjZ8hU_4JtzOyPEpO-wywBw1kKigayEovioH0WGzrKTRp9SIYa6JmaBRw8ALHIFEfEmg7TYsaPGQjH6GxIxDzReOZp-Nb0b6zblmUg-Zrm43Jfnu-O8qhSDoRj7pMVzrBMdfiLzjpI6h_pWRP1MocjZkcHxLu8_Ruj2_L35tRQvzkcH5VzVWBGzT?width=3840&height=1998&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mY0wFowXjJDpscz3weK0VgD2MvZCCa-ITbKoOPLEJqIE8JDtBqfr3EjfCHOFgd_mUFVIOC3voaAY4rLhmUh1_AofN-QyIGBpSGIsqjwl_Nf5teib-NdXeJhI7MXDzKtNBVwFlZye7TwS_Pw5H6mGqAzyVm-r13BiWtTX5xXNcbjsF32OPkunWzRcQ2FU4GicC?width=3264&height=1700&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mOUIG4_wGfZTzcdFHWLOipGlFze3RENp1AHXpDj0_lrdg3TqPLGgbTvWrRHedTR8BakoigkJ_ZZTzVf3Oi_GFvMKnEmUQMmbRMPHLF2sZqcO2yEzsXnBln7djCeqcJLxrcQ6PLr0ZkVXSeE7PNp-wY-vj2tqKsqeZfSQANbQmLEqd2P3_iKyxQk-ahwXlY44e?width=2560&height=1332&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mUz-0nqUxXQa17qOpU-T__ghlWG7OhxnfEoM8AZOlu9umKEDSmqL0WJSfPkzgFZcQ_P4oeHYp2gKytZn9t7b7snFiwk8mp9nQTr5oTEiG-42pQ86tA8xtXBTTDlmMKHp-4lnr44e__BybNPGCPk_mJU4g4DiISq5Jk5zCrpTB3n3HFRDBLcZP180Mqy9qKtPy?width=2560&height=1328&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mdQ7PUDURi6_vfxMz_rY5rvtzNfMs3O6lmsLQUMBmnPS_zXGrbcbokZi4YdYG26a5djWhs6kE6XH_lz-omdG1kkNdw7m9S2sdgo__u7aFDC8a91628vY1DqMHAbu4ooeCCh8-5VjLK50gJH5cAkcU4NvX1H0zRePYGIQXeb1hVlBdbViw-81sl7Dpe5sXy3sn?width=3840&height=1974&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mYusmqvBMxwGIWMIG8ma4jj4ipK5ICdx0ZjYKDVmjiSMmlt_NBC9GmdUg1OG9Kn111xHjx9c1PO8Wl7sLkLoqGJp80owpKmmDNL7Y0LlotrO6dNTBYmek3RKbfB1lI3Ds2N16lWPlmA2fRoT_yRh5ajPqn8-DIa7L2adL48Sy5yIZJwMyJXv2x5aQ7tFk8eoJ?width=3840&height=1989&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mbq7uNVBxmUAuXzEIkXDkTJiZA2MEudN3vVM-JrddCCF0oqx5EaXmw5OgKY4lRrV-bC5OU7E5gwcrtNr-RkohgJibfSkm6I3j7vhCWlLq0nKIp56DKAmMvr11_8Ee71koSUpBx4Ss95W5OJxxIPs44qBIV7QfS6TRgxBP20-fSBHCAvuqCaf7WIyBHrUd8Bja?width=3840&height=2004&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mi7kV6f3-0L6kkoZ4R4144PJXYErakyR_F8oNSH1HKpDmNEjNGflHU5Pser3l1EdHwIcbGBgaQ3ton0v1j5iTdehSrPFkzhEpyWoPKGJ_KkHv5Gh-jnKcS4e6jBhFtG6iKJ726Kgkiyzx8fT4ybWHfAjmw6t_i_455u-Lt4dPZ2OkisjYKNIwQ9Tyez7HoR7c?width=2560&height=1334&cropmode=none"
+      ]
+  },
+  "DarkBeautiful":{
+      "image":[
+          "https://chi01pap001files.storage.live.com/y4mXMWhWv8UGMM6MPe-Hik4IbHB08XlcafM6CLMX__dc8ZyeUm_Cq2RXFBnYqjalXI4g4IUVYdbx5VP0AMwISmakAGis0SO14hSeLL3n2KG6wAkLg26XA525srVXQTr9hglK4Issm7pjX-gRpGl_mZrgxIssl81HAyD3hqUlPHLr-hIvJRajB1u7XmHwkbrd8Kv?width=3840&height=2001&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mG0M3Gsne6F2X2xg8zIah90hyeTdahpFlZ1AGVSy4pq1ym-V2jrBiZjGT6F9aYpQhrErgAXV6Bpm_N21TASPBYg-Ja_HzzKbycgYuQ7wjwscuBUXASZE8XVZmRUTM5mfNsm62YgaAAJ8UwIZEZBfMM0-hoaIBTmbp7X82Hsf_de9umjZ_2_nAMjPrI8XHskjb?width=3840&height=1998&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mUur2Hq9K5o8MDjYsN7mG_G2rnlvwFgRJ9uyGreGHJYNmK2-0rCLvQZOZMobFgGoEga_tOf8f6UCeP0NhBAOZzp0ASJysTllolsUFebuICOj-qMWdeCxwsTkiZt0trFjSpCUHUrjJYBPWf5wuju0OrJy0L4CvN5hcV4Pdt52U4l-UmQWIjt2B6kdyVvCsd5Xe?width=2560&height=1330&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mOfIX-h_nWI5Te8YukC0TbKnMriwZfxmGJoHC0EaqbaI8F-bl-1L_Dpy2OgvMzi9ECOAUoGUvJRXpxmu19239IRxqzumEapXBVzhH7SjWsQ1uJkKAjIKrLO8hVT35v6wTY5o8slNmN2vYWc_r-w20mbEQWG07PG7w9MvCIgzsXgE6MbPPBDln4e2JxbHHYK1_?width=2560&height=1332&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mIHGaj0vZUXumtO8yM_69pxidhZRtmDykmS_rw9yrPJlB0e-oIimY_Cus97tBI9317w6ELz15cXcaE6EILMBvztURez-_dsYUrLr5ea7YwaKXN1PPTwD3m6a_Wmk0baiszrWfZOXABc0mMbRn7aJcUpTZ3db4V1m1TTvMKJbiiAC-SIwZxtxbmJKtEpjoeu9S?width=2560&height=1332&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4myROvo9U6ZyQzwbGIW5i26De_4Tqr7ZVs0EYnhuXltLLHyaK4DTZ99C3bU-SAyjImK_5cTcD1wvmniJ4ZdLCVnSSgzZs7KxUL9DSwaK1jdCf3EpQ6PDqu8Xa6_TxMRdHTTsGGg2zgnSr_uj9q7u-vZLc_sNTaWh18sXCXgFCUrTEKIYZu_Gos4ztxIwnl9dff?width=3840&height=1986&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4m1l17me7X6SeMxYFuol8B7iXMjI0t69EBWPxNNLxrG1XDqq-Lx_fmaffX36rygqYXNTfb8DNLYRhGFaqrO_ONQjoQQePotfQJA0jkxvXA11hlXw6jATucOqVMtzFVE8TWV_0QAT98zYJZKODmVhRLl-M2sDWsrKlv09thanwLnn8PU6xxt0STYGtOexQRTnGz?width=2560&height=1332&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4m-L8CYD0umUMEX3gZHNdFPnqn1XXl4QBWuoR8UgoKgfJlB2YvqSrWR0Hor_pZjm39ez4r9ud1L6edG2avkNwrsjXcHLdGxcYOv0h9jxGl8KRe6an1Fic0AHFPI-vVZfCShfK-ByFtDutR0mazXMUiu5GZ7L0svjwPcEOdX320DBPGKvPMqYf2dkia7ofwmr3D?width=2560&height=1328&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4m_ROFtZ4oSXjmHLJxcYkidG6NCwnHdoRNq9SZnwcxFS4z3uZvGQLaf4U09bOt9_hu8uTi2hdyrIMCuKp7m_cYgDa72mHm3DVoov2C51jmEnNi9JSEvxSJsXwMJoRUyb1FJd50x1yIKwsRiZ98serl04IskOTB-YnWix2gdNQ0c-npJG1pLAZ0tdyTLA_STCcs?width=2560&height=1332&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mt7NJy8jFJefMPD1UERntb3M_UZysjA3JCta1hM7QIrnBYAwkIr7HENlmHknG-OAZhDXBJmZ7hwvlVPAXKNrQJaAr-5GC5Vdhx0KWIjqZAyd2L3_mFhKFB5Wu2kn6X2eyVAThD9DQnDvqESJEelz1Sds0F5LgF8_dwVjvPn6Pn-OBvt1Gt0nvwhJrvRi6u7Bh?width=2560&height=1330&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4mB88ZYkvWm2nU5bM-o_dResYXOaaVKnzsVTKJI3OLeE1v-l-22ONdYSGK4Mn4Dm077NMQTeYH_-tbDtytR5jotbTqFYomDLRChf9_L97tUsaOT1AQJo93azixBYs8xDoujb1YeD2mmZHCKbL3XEPqn93osMnybXhod_k_UIEZ3cp7DlSAjQ2O3W8Br6myEvPo?width=3840&height=1998&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4muSkirwPD3ZbHSOb_jBAHXQ8oHKDoCmY3Zu4nosUXV-Wgm360wCYX8PqIgmpV-7HODDo06Sc9OFVpWhixA5-_yPzzyrn0HA2RFLnQNGd86PxhAy2hGe896ktLcXzfbaZzslzymcGkQ2HSdYRX5T1ZGulpMUrrU4bdckN7HurFBj5BPLjJWqcHjyIKSQrrG5B8?width=3840&height=1977&cropmode=none",
+          "https://chi01pap001files.storage.live.com/y4m81d2uN68FQmG7PT7_sR9XwPQRgcrVdg6oDSp08TIwJ9U_yEguovUljqWqBsPsCn2MIvgm40mjujfKpDm4nLU488jisxlHgOQovWSCeOof9QtWhTYajDmTY5sQ-V7Si3s5i3YMcRyYqH2OyfGAQDBzt3arNs6L5KEoO-lY-07ca8ESHsfmuv2LTQVTu4qsrK7?width=3840&height=2004&cropmode=none"
+      ]
+  }
+}
 
-// 
+
 
 var slideIndex = 1;
-showSlides(slideIndex);
+
+document.addEventListener("DOMContentLoaded",function(){
+  popSlides(images);  
+  showSlides(slideIndex,"ts");
+  showSlides(slideIndex,"lbs");
+  showSlides(slideIndex,"dbs");
+});
+
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
@@ -12,10 +63,10 @@ function currentSlide(n) {
   showSlides(slideIndex = n);
 }
 
-function showSlides(n) {
+function showSlides(n,type) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
+  var slides = document.getElementsByClassName("mySlides " + type );
+  //var dots = document.getElementsByClassName("dot");
   if(n > slides.length) {
     slideIndex = 1
   }
@@ -25,23 +76,25 @@ function showSlides(n) {
   for(i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
-  for(i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
+  //for(i = 0; i < dots.length; i++) {
+  //  dots[i].className = dots[i].className.replace(" active", "");
+  //}
   slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+  //dots[slideIndex - 1].className += " active";
 }
 
 // HTML DOM for Sliders
 
-function addImg(image,name) {
-    const slider = document.querySelector(name);
-    slider.appendChild(image);
+function addImg(imagelink,name) {
+    const slider = document.getElementById(name);
+    const slide = genSlide(imagelink,name);
+    slider.appendChild(slide);
 }
 
-function genSlide(link) {
+function genSlide(link,name) {
     const slide = document.createElement("div");
-    slide.className = "mySlides fade";
+    slide.className = "mySlides fade " + name;
+
     // add numbers for image
     slide.appendChild(genImg(link));
     // add name for image
@@ -54,16 +107,44 @@ function genImg(link) {
     return image;
 }
 
-function popSlides(images) {
-  for (i=0;i<images.Togetherness.image.length(); ++i) {
-    addImg(images.Togetherness.image[i],"#togetherness-slideshow");
-  }
-
-  for (i=0;i<images.LivingBreathing.image.length();++i){
-    addImg(images.LivingBreathing.image[i],"#livingbreathing-slideshow");
-  }
-
-  for (i=0;i<images.DarkBeautiful;++i) {
-    addImg(images.DarkBeautiful.image[i],"#darkbeautiful-slideshow");
-  }
+function genPrev() {
+  const prev = document.createElement("a");
+  prev.className = "prev";
+  prev.addEventListener("click",function(){
+    plusSlides(-1);
+  });
+  return prev;
 }
+
+function genNext() {
+  const next = document.createElement("a");
+  next.className = "next";
+  next.addEventListener("click",function(){
+    plusSlides(1);
+  });
+  return next;
+}
+
+function addArrows(name) {
+  const element = document.getElementById(name);
+  element.appendChild(genNext());
+  element.appendChild(genPrev());
+}
+
+function popSlides(images) {
+  for (i=0;i<images.Togetherness.image.length; ++i) {
+    addImg(images.Togetherness.image[i],"ts");
+  }
+  addArrows("ts");
+
+  for (i=0;i<images.LivingBreathing.image.length;++i){
+    addImg(images.LivingBreathing.image[i],"lbs");
+  }
+  addArrows("lbs");
+
+  for (i=0;i<images.DarkBeautiful.image.length;++i) {
+    addImg(images.DarkBeautiful.image[i],"dbs");
+  }
+  addArrows("dbs");
+}
+
